7fbe0d2c87ecd0cc1fc8f7209d4693da54f5c9a9
Akshit rai <raiakshit04@gmail.com>
2026-05-01 02:25:08 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -12,15 +12,12 @@ def load_json_folder(folder, key_name):
         with open(os.path.join(path, file), "r", encoding="utf-8") as f:
             obj = json.load(f)
 
-            # 🔥 Use correct key
             key = obj.get(key_name)
             if key:
                 data[key] = obj
 
     return data
 
-
-# 🔥 Correct key mappings
 MERCHANTS = load_json_folder("merchants", "merchant_id")
 TRIGGERS = load_json_folder("triggers", "trigger_id")
 CUSTOMERS = load_json_folder("customers", "customer_id")
